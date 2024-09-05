70bbfecadd71d392061b511b7e859dcd95598843
DuvanLT <duvanred0@gmail.com>
2024-09-05 18:50:07 -0500

    uso de hash tambien para su registro, contraseña asegurada, ademas de inicio de sesion realizado

--- iniciar.php
+++ iniciar.php
@@ -6,17 +6,25 @@ $conectar = conectar();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombre = $_POST['nombre'];
     $contraseña = $_POST['contraseña'];
-
     $hashed_password = password_hash($contraseña, PASSWORD_DEFAULT);
-
     $stmt = $conectar->prepare("INSERT INTO registro (nombre, contraseña) VALUES (?, ?)");
     $stmt->bind_param("ss", $nombre, $hashed_password);
+    $stmt->execute();
+    $stmt->store_result();
 
-    if ($stmt->execute()) {
-        echo "Registro exitoso!";
+    if ($stmt->num_rows === 1) {
+        $stmt->bind_result($hashed_password);
+        $stmt->fetch();
+
+        if (password_verify($contraseña, $hashed_password)) {
+            echo "Inicio de sesión exitoso!";
+        } else {
+            echo "Contraseña incorrecta.";
+        }
     } else {
-        echo "Error al enviar los datos: " . $stmt->error;
+        echo "Usuario no encontrado.";
     }
+
     $stmt->close();
 }
 ?>
