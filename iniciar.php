a55dabc6cee46d9467fbfcfb562706661e26007a
DuvanLT <duvanred0@gmail.com>
2024-09-06 00:56:52 -0500

    junto al id del usuario se realiza el inicio de sesion

--- iniciar.php
+++ iniciar.php
@@ -1,30 +1,37 @@
 <?php
 include("conectar.php"); 
+session_start(); 
 
 $conectar = conectar(); 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombre = $_POST['nombre'];
     $contraseña = $_POST['contraseña'];
-    $hashed_password = password_hash($contraseña, PASSWORD_DEFAULT);
-    $stmt = $conectar->prepare("INSERT INTO registro (nombre, contraseña) VALUES (?, ?)");
-    $stmt->bind_param("ss", $nombre, $hashed_password);
-    $stmt->execute();
-    $stmt->store_result();
 
-    if ($stmt->num_rows === 1) {
-        $stmt->bind_result($hashed_password);
-        $stmt->fetch();
-
-        if (password_verify($contraseña, $hashed_password)) {
-            echo "Inicio de sesión exitoso!";
-        } else {
-            echo "Contraseña incorrecta.";
-        }
-    } else {
-        echo "Usuario no encontrado. Si no tienes una cuenta Registrate <a href='register.html'>aqui</a>";
+    if (empty($nombre) || empty($contraseña)) {
+        echo 'No se ha iniciado sesión.';
+        exit();
     }
 
+    $stmt = $conectar->prepare("SELECT id, contraseña FROM registro WHERE nombre = ?");
+    $stmt->bind_param("s", $nombre);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($id, $hashed_password);
+
+    if ($stmt->num_rows > 0) {
+        $stmt->fetch();
+        
+        if (password_verify($contraseña, $hashed_password)) {
+            $_SESSION['user_id'] = $id;
+            $_SESSION['nombre'] = $nombre;
+            echo 'success'; 
+        } else {
+            echo 'Contraseña incorrecta.';
+        }
+    } else {
+        echo 'Usuario no encontrado.';
+    }
     $stmt->close();
 }
 ?>
